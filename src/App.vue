bfe76ddab39e5965f6d8cf25895931b8eb36efa2
Jereu <jastudim@outlook.com>
2018-09-27 22:35:37 +0100

    pokeball image added

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,11 @@
       <section class="jumbotron text-center">
         <div class="container">
           <h1 class="jumbotron-heading">Poke-Vue</h1>
+              <img src="./assets/pokeball.png" class="img-fluid" alt="..." id="pokeball-image">
+          <br>
           <p class="lead text-muted">Basic information about the Pokémons you love.</p>
           <p>
-            <a href="#" class="btn btn-primary my-2">Loading...</a>
+            <!--<a href="#" class="btn btn-primary my-2">Loading...</a>-->
           </p>
         </div>
       </section>
@@ -23,9 +25,9 @@
 </template>
 
 <script>
-import HeaderApp from "./components/HeaderApp.vue"
-import PokemonGridApp from "./components/PokemonGridApp.vue"
-import FooterApp from "./components/FooterApp.vue"
+import HeaderApp from "./components/HeaderApp.vue";
+import PokemonGridApp from "./components/PokemonGridApp.vue";
+import FooterApp from "./components/FooterApp.vue";
 
 export default {
   name: "app",
@@ -34,7 +36,7 @@ export default {
     PokemonGridApp,
     FooterApp
   }
-}
+};
 </script>
 
 <style>
@@ -42,5 +44,8 @@ footer {
   padding-top: 3rem;
   padding-bottom: 3rem;
   background-color: #e9ecef;
+}
+#pokeball-image {
+  width: 10%;
 }
 </style>
